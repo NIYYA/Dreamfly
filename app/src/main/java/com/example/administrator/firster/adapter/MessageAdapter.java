decba824433e077ac18990906eb0550fce4f6849
unknown <>
2017-03-28 10:01:49 +0800

    Ad

---
+++ app/src/main/java/com/example/administrator/firster/adapter/MessageAdapter.java
@@ -0,0 +1,8 @@
+package com.example.administrator.firster.adapter;
+
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+
+public class MessageAdapter {
+}
--- app/src/main/java/com/example/administrator/firster/fragment/Message.java
+++ app/src/main/java/com/example/administrator/firster/fragment/Message.java
@@ -1,8 +1,39 @@
 package com.example.administrator.firster.fragment;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.administrator.firster.R;
+
 /**
  * Created by Administrator on 2017/3/27.
  */
 
-public class Message {
+public class Message extends Fragment {
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+         view = inflater.inflate(R.layout.fragment_message, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        init();
+    }
+
+    private void init() {
+        ListView listView = (ListView) view.findViewById(R.id.message_list);
+
+
+    }
 }
